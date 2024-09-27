0e8afd08f77aa6fbb2fd5991ecbd7bdc51a430bf
Jasper-afk <jasper.cullen@my.jcu.edu.au>
2024-09-27 10:46:15 +1000

    Refactor score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,25 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+"""CP1404 practical 2 determine score program."""
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    """Get score from the user and print the score category."""
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+    print(result)
+    result = determine_score(random.randint(1, 100))
+    print(f"Random score: {result}")
+
+def determine_score(score: float) -> str:
+    """Determine the score category based on input."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    if score >= 90:
+        return "Excellent"
+    if score >= 50:
+        return "Pass"
+    return "Bad"
+
+
+main()
