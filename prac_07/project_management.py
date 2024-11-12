6d101bb4bca6b91574bf4244afb6ad780b97d3e8
Jasper-afk <jasper.cullen@my.jcu.edu.au>
2024-11-12 23:16:18 +1000

    Add functional 'save' and 'load' menu options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -22,11 +22,43 @@ def main():
     print("Welcome to Pythonic Project Management")
 
     projects = load_project_data(DEFAULT_FILENAME)
-    print(f"{len(projects)} loaded from {DEFAULT_FILENAME}")
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
 
     menu_choice = input(f"{MENU}\n>>> ").upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            filename = get_valid_filename()
+            projects = load_project_data(filename)
+            print(f"Loaded {len(projects)} projects from {filename}")
+        elif menu_choice == "S":
+            filename = get_valid_filename()
+            write_project_data(filename, projects)
+            print(f"{len(projects)} projects successfully saved to {filename}")
+        elif menu_choice == "D":
+            pass
+        elif menu_choice == "F":
+            pass
+        elif menu_choice == "A":
+            pass
+        elif menu_choice == "U":
+            pass
+        else:
+            print("Invalid menu choice.")
+        menu_choice = input(f"{MENU}\n>>> ").upper()
 
+def get_valid_filename():
+    filename = input("Enter filename (with extension): ")
+    while not filename: # Checks if the user entered a non-empty string.
+        print("Invalid filename")
+        filename = input("Enter filename (with extension): ")
+    return filename
 
+def write_project_data(filename, data):
+    """Write project data to a file as CSV format."""
+    with open(filename, "w") as out_file:
+        for record in data:
+            print(f"{record.name}\t{record.start_date}\t{record.priority}\t{record.cost_estimate}\t{
+            record.completion_percentage}\t", file=out_file)
 
 def load_project_data(filename):
     """Load project data from CSV file."""
