2e3291bac39212d6ffa6e35b012767a0fc942890
Jasper-afk <jasper.cullen@my.jcu.edu.au>
2024-11-18 16:26:38 +1000

    Add docstrings to Project class methods

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,6 +6,7 @@ class Project:
     """Represent what will be in a Project object."""
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise an instance of the Project class."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -13,5 +14,6 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __repr__(self):
+        """Return official string representation of a Project object."""
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${
         self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
